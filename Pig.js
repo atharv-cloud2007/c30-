0a769e2ca8476e919dee2519006482ef8d940909
atharv-cloud2007 <73945573+atharv-cloud2007@users.noreply.github.com>
2021-01-03 17:40:07 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,21 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.visible=255
+  }
+display(){
+  
+  if(this.body.speed < 2.9){
+    super.display()
+  }
+else{
+  World.remove(world,this.body)
+  push()
+  this.visible=this.visible-3
+  tint(255,this.visible)
+  image(this.image,this.body.position.x,this.body.position.y,50,50)
+  pop()
+}
+}
+};
